8768f295d71a96d3d64cf97b9c15d282fc96d798
ng.kiat <ng.kiat@mranti.my>
2026-02-15 00:35:04 +0800

    test scroing1

---
+++ api/scoring.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once __DIR__ . '/_helpers.php';
+
+handle_options();
+require_method('GET', 'POST');
+
+$formId = require_param('form_id');
+$applicationId = require_param('application_id');
+
+$id = require_param('id');
+$client = get_client();
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $filter = array(
+        "applicationId" => $applicationId
+    );
+    $score = $client->getFormSubmissions($formId,0,0, $filter);
+    print "<pre>";
+    print_r($score);
+    exit;
+}
+
+try {
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $submission = $client->getSubmission($id);
+        json_response(['submission' => $submission]);
+    } else {
+        $body = json_decode(file_get_contents('php://input'), true);
+        if (!$body || !is_array($body)) {
+            error_response('Request body must be a JSON object with field_id: value pairs');
+        }
+        $result = $client->editSubmission($id, $body);
+        json_response(['result' => $result]);
+    }
+} catch (Exception $e) {
+    error_response($e->getMessage(), $e->getCode() ?: 500);
+}
