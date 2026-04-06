2e76fa1bbea125c7242d08134f209dc2d291d172
ng.kiat <ng.kiat@mranti.my>
2026-04-06 10:10:58 +0800

    add new assignee

--- api/assign.php
+++ api/assign.php
@@ -26,7 +26,7 @@ $targetFormId = '260193165468058';
 //$targetFormId = '260492349743464';
 
 // The 4 available reviewers
-$reviewers = ['alya@mranti.my', 'badzlan.khan@mranti.my', 'muhammadhafiz.h@mranti.my', 'sharienna@mranti.my'];
+$reviewers = ['alya@mranti.my', 'badzlan.khan@mranti.my', 'muhammadhafiz.h@mranti.my', 'sharienna@mranti.my','rossuriana@mranti.my','zulfikri@mranti.my'];
 //$reviewers = ['ng.kiat@mranti.my', 'ng_king@yahoo.com', 'ngking80@gmail.com', 'derekfoo87@gmail.com'];
 $client = get_client();
 
